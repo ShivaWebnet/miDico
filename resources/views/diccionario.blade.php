e25bd4b06d5041d4b84c67b050e3af28c8afca11
ShivaWebnet <shivawebnet@gmail.com>
2017-04-05 16:52:03 +0200

    order tables np

--- resources/views/diccionario.blade.php
+++ resources/views/diccionario.blade.php
@@ -7,16 +7,16 @@
           <table>
             <thead>
               <th>Natal</th>
-              <th>Idioma</th>
               <th>Traduccion</th>
+              <th>Idioma</th>
               <th>categoria</th>
               <th>acciones</th>
             </thead>
             @foreach($expresiones as $i)
               <tr>
                 <td>{{ $i->materna }}</td>
-                <td>{{ $i->idioma }}</td>
                 <td>{{ $i->expresion }}</td>
+                <td>{{ $i->idioma }}</td>
                 <td>{{ $i->categoria }}</td>
                 <td><a href="diccionario/edit/{{$i->id}}">Editar</a></td>
                 <td><a href="diccionario/borra/{{$i->id}}">Borrar</a></td>
